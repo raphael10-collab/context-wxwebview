f33fd8441bcc434bb0c1acc93c44fb417568785c
raphy <raphael.stonehorse@gmail.com>
2024-07-15 19:03:21 +0200

    commit

--- context-webview.cpp
+++ context-webview.cpp
@@ -4,12 +4,6 @@
 
 //MyFrame(wxWindow* parent = nullptr) : wxFrame(parent, wxID_ANY, "Test")
 
-enum
-{
-    Show_Transparent = 1,
-    Show_TransparentBg = 2
-};
-
 
 MyFrame::MyFrame(wxWindow* parent) : wxFrame(parent, wxID_ANY, "Test")
 {
@@ -27,21 +21,10 @@ MyFrame::MyFrame(wxWindow* parent) : wxFrame(parent, wxID_ANY, "Test")
 
     wxMenuBar* const mbar = new wxMenuBar;
     wxMenu* const menuFrames = new wxMenu;
-    menuFrames->Append(Show_Transparent, "Show &transparent window\tCtrl-T");
 
     SetMenuBar(mbar);
     Show();
 };
-
-
-SeeThroughFrame::~SeeThroughFrame() {};
-
-wxBEGIN_EVENT_TABLE(SeeThroughFrame, wxFrame)
-    EVT_PAINT(SeeThroughFrame::OnPaint)
-wxEND_EVENT_TABLE()
-
-
-
 
 
 void MyFrame::OnWebViewLoaded(wxWebViewEvent&)
@@ -100,38 +83,6 @@ void MyFrame::OnAbout (wxCommandEvent &WXUNUSED(event))
     dialog.ShowModal();
 
 };
-
-void SeeThroughFrame::OnAlpha(wxCommandEvent& event)
-{
-    SetTransparent(event.GetInt());
-}
-
-
-// Paints a grid of varying hue and alpha
-void SeeThroughFrame::OnPaint(wxPaintEvent& WXUNUSED(evt))
-{
-    wxPaintDC dc(this);
-    std::unique_ptr<wxGraphicsContext> gc(wxGraphicsContext::Create(dc));
-
-    // Draw 3 bands: one opaque, one semi-transparent and one transparent.
-    const wxSize size = GetClientSize();
-    const double h = size.y / 3.0;
-
-    wxGraphicsPath path = gc->CreatePath();
-    path.AddRectangle(0, 0, size.x, h);
-    gc->SetBrush(wxBrush(wxColour(0, 0, 255, wxALPHA_OPAQUE)));
-    gc->FillPath(path);
-
-    gc->SetBrush(wxBrush(wxColour(0, 0, 255, wxALPHA_OPAQUE / 2)));
-    gc->Translate(0, h);
-    gc->FillPath(path);
-
-    // This blue won't actually be seen and instead the white background will
-    // be visible, because this brush is fully transparent.
-    gc->SetBrush(wxBrush(wxColour(0, 0, 255, wxALPHA_TRANSPARENT)));
-    gc->Translate(0, h);
-    gc->FillPath(path);
-}
 
 
 
